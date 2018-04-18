cfa8b1f7bb1fade436d55a1a0e30a3bc33e9f3c2
lechu7 <lechu0@vp.pl>
2018-04-18 22:24:57 +0200

    Pierwszy skrypt

---
+++ Firefox_Driver/Readme.txt
@@ -0,0 +1,6 @@
+Skopiuj folder tmp na dysk D aby dzia³a³o.
+
+
+Je¿eli nie masz dysku C to skopiuj w inne miejsce i zmieñ œcie¿kê w tej lini:
+FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"d:\tmp");
+
--- Testy/Program.cs
+++ Testy/Program.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
 
 namespace Testy
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Testujemy");
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"d:\tmp");
+            service.FirefoxBinaryPath= @"C:\Programy\Mozilla Firefox\firefox.exe";
+            IWebDriver driver = new FirefoxDriver(service);
+            driver.Navigate().GoToUrl("http://www.google.pl");
+
+
+            //Deklaracja obiktów
+            IWebElement input = driver.FindElement(By.Id("lst-ib"));
+            IWebElement button = driver.FindElement(By.Name("btnK"));
+
+
+            //Metody
+            input.SendKeys("BookWeb_SIAG");
+            button.Click();
+
             Console.ReadKey();
         }
     }
