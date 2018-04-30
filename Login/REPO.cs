80a8c89144e6ffaa903ab5cc2fd35d814ddfd0e2
lechu7 <lechu0@vp.pl>
2018-04-30 23:01:25 +0200

    SkasowanieNiepotrzebnejPlaylisty

--- Login/REPO.cs
+++ Login/REPO.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 
 
-    public static class REPO
-    {
-        //Przyciski górnego menu 
-        public static By TB_UpMain_login = By.Id("login");
-        public static By TB_UpMain_addBook = By.Id("addbook");
+public static class REPO
+{
+    //Przyciski górnego menu 
+    public static By TB_UpMain_login = By.Id("login");
+    public static By TB_UpMain_addBook = By.Id("addbook");
 
-        //Strona główna po zalogowaniu
-        public static By PUWin_home_correctLogin = By.XPath("//div[contains(.,'Zostałeś prawidłowo zalogowany')]");
+    //Strona główna po zalogowaniu
+    public static By PUWin_home_correctLogin = By.XPath("//div[contains(.,'Zostałeś prawidłowo zalogowany')]");
 
-        //Strona logowania
-        public static By ET_login_mail = By.Id("email");
-        public static By ET_login_password = By.Id("password");
-        public static By PUWin_login_incorrectLoginDetails = By.Id("flash_danger");
-        public static By BT_login_logIn = By.Id("submit");
-       
-    }
+    //Strona logowania
+    public static By ET_login_mail = By.Id("email");
+    public static By ET_login_password = By.Id("password");
+    public static By PUWin_login_incorrectLoginDetails = By.XPath("//div[contains(.,'Podałeś błędne dane logowania')]");
+    public static By BT_login_logIn = By.Id("submit");
+
+}
 
