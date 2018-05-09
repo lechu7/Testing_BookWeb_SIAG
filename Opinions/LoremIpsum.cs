1da5f0a043283582a70ed2ff9c52165d0b1a5d9a
Unknown <michal11111995@gmail.com>
2018-05-09 23:59:31 +0200

    Testy opinii 7-10

---
+++ Opinions/LoremIpsum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opinions
+{
+   static class LoremIpsum
+    {
+       public static string lo = "Lorem ipsum dolor sit amet, cum eu novum affert tritani, eum tota deleniti no. Eam legere detracto disputando in. Cu odio hinc civibus duo, audiam fabulas ex eum, cetero impedit ad usu. Sea nobis putant ut, sed diceret antiopam ea. Te quo discere electram posidonium, ad sea aeque oblique periculis. \nCu falli labitur percipit nam, alii tincidunt per cu, ad probo erant tritani quo.Id dicta antiopam eos, ad dolore ignota mnesarchum sit.Eu debitis incorrupte nam. Eu vel quis nibh hendrerit, dicit cotidieque mea an.\n Nam quis aliquid albucius ex.Ne sed case quando facilisis, has erat equidem te, purto malis deterruisset te per.Eum ei eruditi urbanitas. Wisi placerat te pro, ne nam assum insolens dissentias.Solum detraxit partiendo eu his. \nQuo nulla maluisset ex, causae maiorum partiendo ut sed.Ignota pericula suscipiantur ea mea.Integre definiebas reformidans vim ut, case nonumes percipitur per ad. Mel ex vivendum deserunt. Per cu dicta fabellas apeirian, his probatus maluisset definitiones in, ea eripuit docendi blandit duo. \nMeliore posidonium an pro. Est ei illum tollit civibus, et sea utamur tibique, sit omnis iracundia at. Pro ut habeo soleat. Ad pri perfecto praesent, ei probo fabulas molestie sit.Esse mutat patrioque in duo, pri ne altera dignissim, labore inimicus qui ne. Modus tincidunt complectitur et eam.An pri affert consulatu similique, ex tollit sententiae eam.\nMolestie expetenda his at, his primis habemus no. Errem oportere inciderint ei sed, numquam definiebas cum at. Id dicta viris adolescens usu.Mei ea possit iriure option.";
+    }
+}
--- Opinions/TestClass.cs
+++ Opinions/TestClass.cs
@@ -201,7 +201,238 @@ namespace Opinions
             //zaimplementować jak Daniel wprowadzi zmiany do profilu!!!!
             throw new NotImplementedException("test nie zaimplementowany!");
         }
+        [Test]
+        public void Opinion7()//sprawdzenie czy można dać opis opinii o długości powyżej 1000 znaków
+        {
+            //logowanie
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            Thread.Sleep(1500);
+            mbl.EnterMail(driver, REPO.loginUserTest, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passUserTest, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+            Thread.Sleep(500);
 
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //dodanie opinii
+            bp.Add_description(driver, REPO.TA_books_page_description, LoremIpsum.lo);
+            bp.Add_rate(driver, REPO.SE_books_page_opinionRate, 0);
+            bp.Submit_opinion(driver, REPO.BT_books_page_submit);
+
+            //sprawdzenie, czy opinia jest na stronie książki
+            try
+            {
+                driver.FindElement(By.XPath("//div[contains(.,'test')]"));
+            }
+            catch (Exception)
+            {
+
+                Assert.Pass();
+            }
+            Assert.Fail();
+            
+        }
+
+        [Test]
+        public void Opinion8()//usunięcie swojej opinii
+        {
+            //logowanie
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            Thread.Sleep(1500);
+            mbl.EnterMail(driver, REPO.loginUserTest, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passUserTest, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+            Thread.Sleep(500);
+
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //dodanie opinii
+            bp.Add_description(driver, REPO.TA_books_page_description, "nie polecam");
+            bp.Add_rate(driver, REPO.SE_books_page_opinionRate, 0);
+            bp.Submit_opinion(driver, REPO.BT_books_page_submit);
+
+            throw new NotImplementedException("test nie zaimplementowany!");
+        }
+
+        [Test]
+        public void Opinion9()//usuwanie opinii jako admin  
+        {
+            //logowanie
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            Thread.Sleep(1500);
+            mbl.EnterMail(driver, REPO.loginUserTest, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passUserTest, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+            Thread.Sleep(500);
+
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //dodanie opinii
+            bp.Add_description(driver, REPO.TA_books_page_description, "nie polecam");
+            bp.Add_rate(driver, REPO.SE_books_page_opinionRate, 0);
+            bp.Submit_opinion(driver, REPO.BT_books_page_submit);
+
+            //wylogowanie
+            driver.Navigate().GoToUrl(REPO.side);
+            driver.FindElement(REPO.TB_UpMain_logOut).Click();
+
+            //logowanie jako admin
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            mbl.EnterMail(driver, REPO.mailAdmin, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passAdmin, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //usuwanie opinii użytkownika test
+            try
+            {
+                while (true)
+                {
+                    driver.FindElement(By.XPath("//div[contains(.,'test')]/a[contains(.,'Usuń')]")).Click();
+                }
+
+            }
+            catch (Exception) { }
+            //sprawdzenie
+            try
+            {
+                driver.FindElement(REPO.DIV_books_page_rateTest);
+            }
+            catch (Exception)
+            {
+
+                Assert.Pass();
+            }
+            Assert.Fail();
+
+        }
+
+        [Test]
+        public void Opinion10()//sprawdzenie poprawności średniej ocen
+        {
+            //logowanie jako admin
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            mbl.EnterMail(driver, REPO.mailAdmin, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passAdmin, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //usuwanie opinii użytkowników
+            try
+            {
+                while (true)
+                {
+                    driver.FindElement(By.XPath("//a[contains(.,'Usuń')]")).Click();
+                }
+
+            }
+            catch (Exception) { }
+
+            //wylogowanie
+            driver.Navigate().GoToUrl(REPO.side);
+            driver.FindElement(REPO.TB_UpMain_logOut).Click();
+
+
+
+            //UŻYTKOWNIK TEST
+            //logowanie
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            Thread.Sleep(1500);
+            mbl.EnterMail(driver, REPO.loginUserTest, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passUserTest, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+            Thread.Sleep(500);
+
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //dodanie opinii
+            bp.Add_description(driver, REPO.TA_books_page_description, "nie polecam");
+            bp.Add_rate(driver, REPO.SE_books_page_opinionRate, 0);
+            bp.Submit_opinion(driver, REPO.BT_books_page_submit);
+
+            //wylogowanie
+            driver.Navigate().GoToUrl(REPO.side);
+            driver.FindElement(REPO.TB_UpMain_logOut).Click();
+
+
+            //UŻYTKOWNIK DJAROS
+            //logowanie
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            Thread.Sleep(1500);
+            mbl.EnterMail(driver, REPO.loginUser1, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passUser1, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+            Thread.Sleep(500);
+
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //dodanie opinii
+            bp.Add_description(driver, REPO.TA_books_page_description, "nie polecam");
+            bp.Add_rate(driver, REPO.SE_books_page_opinionRate, 3);
+            bp.Submit_opinion(driver, REPO.BT_books_page_submit);
+
+            //wylogowanie
+            driver.Navigate().GoToUrl(REPO.side);
+            driver.FindElement(REPO.TB_UpMain_logOut).Click();
+
+
+
+            //UŻYTKOWNIK JKWITEK
+            //logowanie
+            mbl.ClickLoginTab(driver, REPO.TB_UpMain_login);
+            Thread.Sleep(1500);
+            mbl.EnterMail(driver, REPO.loginUser2, REPO.ET_login_mail);
+            mbl.EnterPassword(driver, REPO.passUser2, REPO.ET_login_password);
+            mbl.ClickLogIn(driver, REPO.BT_login_logIn);
+            Thread.Sleep(500);
+
+            //przejście do listy książek
+            driver.FindElement(REPO.TB_UpMain_books).Click();
+
+            //wybranie Autobiografia
+            bl.Click_on_book_Autobiografia(driver, REPO.BT_book_Autobiografia);
+
+            //dodanie opinii
+            bp.Add_description(driver, REPO.TA_books_page_description, "nie polecam");
+            bp.Add_rate(driver, REPO.SE_books_page_opinionRate, 4);
+            bp.Submit_opinion(driver, REPO.BT_books_page_submit);
+
+            //wylogowanie
+            driver.Navigate().GoToUrl(REPO.side);
+            driver.FindElement(REPO.TB_UpMain_logOut).Click();
+
+            Assert.Pass();
+        }
 
         [TearDown]
         public void TearDown()
@@ -234,6 +465,8 @@ namespace Opinions
             }
             catch (Exception) { }
 
+            
+         
 
             driver.Quit();
 
